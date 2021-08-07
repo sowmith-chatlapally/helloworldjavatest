47bac4f60a95ffeecb744e1932e577158df16281
sowmith-chatlapally <85552933+sowmith-chatlapally@users.noreply.github.com>
2021-08-07 11:48:27 +0530

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -3,5 +3,6 @@ public class helloworldjavatest{
 	public static void main(String[] args){
 		
 		System.out.println("Hello World - polling test by jenkins for star agile");
+		System.out.println("this is my first CI");
 	}
 }
